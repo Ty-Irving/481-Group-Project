7dedbd9f6dc605b02d22a194b7a7d29d0d13632b
Quinn Ceplis <QCeplis@icloud.com>
2022-11-27 22:48:40 -0700

    Add more common ingredients

--- 481 Portfolio/CookingInstructor/Data/CommonIngredients.cs
+++ 481 Portfolio/CookingInstructor/Data/CommonIngredients.cs
@@ -2,8 +2,21 @@ public class CommonIngredients
 {
     private static readonly List<String> commonIngredients_ = new List<String> {
         "Garlic",
-        "Onion"
+        "Onion",
+        "Butter",
+        "Egg",
+        "Olive Oil",
+        "Flour",
+        "Sugar",
+        "Milk",
+        "Rice",
+        "Soya Sauce",
+        "Bread"
     };
+
+    public List<String> Get() {
+        return commonIngredients_;
+    }
 
     public bool Contains(String name) {
         return commonIngredients_.Contains(name);
